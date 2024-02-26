038421bb5aa3e96a626fab63d755b21150dde942
Nikolai Nemkov <144113116+Nikolai-Nemkov@users.noreply.github.com>
2024-02-26 12:29:36 +0300

    Add files via upload

---
+++ View/StudentView.java
@@ -0,0 +1,14 @@
+package View;
+
+
+import Model.Student;
+
+public class StudentView {
+    public void printStudent(Student student){
+        System.out.println("Студент: " + student);
+    }
+    public void printAllStudent(){
+
+    }
+
+}
